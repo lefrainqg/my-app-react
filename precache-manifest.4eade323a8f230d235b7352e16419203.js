b41e94f1ffa90bfd66a2a221ba2ce413e7cc169b
Efrain QG <lefrainqg@gmail.com>
2018-12-19 17:14:47 -0500

    Despliegue de app React JS

---
+++ precache-manifest.4eade323a8f230d235b7352e16419203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "554da733691ca67df70c1f1df78921be",
+    "url": "/my-app-react/static/media/abc.554da733.png"
+  },
+  {
+    "revision": "c1a0d1e2dc415a3527d0",
+    "url": "/my-app-react/static/js/runtime~main.c1a0d1e2.js"
+  },
+  {
+    "revision": "9c8c9030187e9f1b9e4c",
+    "url": "/my-app-react/static/js/main.9c8c9030.chunk.js"
+  },
+  {
+    "revision": "0a43806805727cce1ef2",
+    "url": "/my-app-react/static/js/1.0a438068.chunk.js"
+  },
+  {
+    "revision": "9c8c9030187e9f1b9e4c",
+    "url": "/my-app-react/static/css/main.3eb96114.chunk.css"
+  },
+  {
+    "revision": "0a43806805727cce1ef2",
+    "url": "/my-app-react/static/css/1.cd51a017.chunk.css"
+  },
+  {
+    "revision": "d01304305efd9c80a7feba9f0635004d",
+    "url": "/my-app-react/index.html"
+  }
+];
